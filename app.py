b1523f477258657154a9105e2ff2320d8c3fdf20
PRABHMEHAR BEDI <prabhmehar2509@gmail.com>
2024-10-17 16:09:00 +0530

    update

--- app.py
+++ app.py
@@ -109,5 +109,9 @@ def predict():
         # Render the result page with the image and prediction
         return render_template('result.html', result=result, probability=pred_prob, image_filename=filename)
 
+# Local Machine
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=5002)
+
+# if __name__ == "__main__":
+#     app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
